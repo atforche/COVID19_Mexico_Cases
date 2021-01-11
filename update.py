25a65c5ef67f309c6a7a836d5f3559c19fc96800
atforche <atforche@gmail.com>
2021-01-11 13:32:43 -0500

    Update

--- update.py
+++ update.py
@@ -1,11 +1,17 @@
 import requests
 from datetime import datetime, timedelta
 
-yesterday = (datetime.today() - timedelta(days=1)).strftime("%Y%m%d")
+yesterday = (datetime.today() - timedelta(days=2)).strftime("%Y%m%d")
 mexico_file_name = "https://datos.covid-19.conacyt.mx/Downloads/Files/Casos_Diarios_Municipio_Confirmados_" + yesterday + ".csv"
 
 mexico_cases = requests.get(mexico_file_name)
-with open("mexico_cases.csv", 'wb') as f:
-    f.write(mexico_cases.content)
+try:
+    mexico_cases.raise_for_status()
+    with open("mexico_cases.csv", 'wb') as f:
+        f.write(mexico_cases.content)
+    print("Cases Successfully Updated at " + datetime.today().strftime("%m/%d/%Y, %H:%M:%S"))
 
-print("Cases Successfully Updated at " + datetime.today().strftime("%m/%d/%Y, %H:%M:%S"))
+except requests.exceptions.HTTPError as err:
+    print("File could not be updated")
+    exit(1)
+
